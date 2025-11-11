760f90fbad9c7ff66ddcf42f60615b9536e4d542
Marc-Andre Seguin <marcandre.seguin@hey.com>
2025-11-11 10:11:22 -0500

    Added helper function createGrid() and defaults to 16 squares. Also added button with prompt for user to set grid size between 1-100

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const container = document.querySelector("#container");
+const resetBtn=document.querySelector('#resetBtn');
 
 
-container.innerHTML=''
+function createGrid(size) {
+  container.innerHTML = "";
+  const squareSize= 960 / size;
 
-for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < size * size; i++) {
     const newSquare = document.createElement("div");
-    const squareSize = 960 / 4
-
-    newSquare.classList.add('singleSquare');
-    newSquare.style.width=`${squareSize}px`;
-    newSquare.style.height=`${squareSize}px`;
+    newSquare.classList.add("singleSquare");
+    newSquare.style.width = `${squareSize}px`;
+    newSquare.style.height = `${squareSize}px`;
     container.appendChild(newSquare);
+  }
 }
+createGrid(16) // default upon loading
 
+
+resetBtn.addEventListener('click', () =>{
+    let newSize=prompt('Enter new grid size (max 100):');
+    newSize = parseInt(newSize);
+    if (newSize > 0 && newSize <= 100) {
+        createGrid(newSize);
+    } else{
+        alert('Please enter a number between 1 and 100');
+    }
+
+});
